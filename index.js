b2f606d5aebfeef18b72d4c9c58babd56a573d9c
ceboulba <antoine@archipicture.fr>
2020-04-17 22:51:56 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter</h1>
+<a sr:"./panoVR_output/index.html">link</a>`;
