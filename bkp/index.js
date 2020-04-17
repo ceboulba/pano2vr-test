f6513299460f00be0e41635eea23836333c8befb
ceboulba <antoine@archipicture.fr>
2020-04-17 22:57:49 +0200

    3 files added, 3 deleted

---
+++ bkp/index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>
+<ul>
+<li>
+<a href:"./panoVR_output/index.html">link</a>
+</li>
+<li>
+<a href:"./panoVR_output/index.html">link</a>
+</li>
+<li>
+<a href:"https://www.archipicture.fr">archipicture</a>
+</li>
+</ul>
+`;
--- index.js
+++
@@ -1,7 +0,0 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>
-<a sr:"./panoVR_output/index.html">link</a>`;
